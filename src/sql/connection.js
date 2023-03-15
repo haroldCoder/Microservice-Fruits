7f6918662439950a33374a12f6937d3affde25ed
haroldCoder <haroldc2005@gmail.com>
2023-03-15 14:06:25 -0500

    config

--- src/sql/connection.js
+++ src/sql/connection.js
@@ -1,11 +1,19 @@
 import mysql from 'mysql2';
+import {
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    DB_PORT
+  } from '../config.js'
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "2364144",
-    database: "fruit"
-});
+const connection = mysql.createPool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
+})
 
 export default connection;
 
