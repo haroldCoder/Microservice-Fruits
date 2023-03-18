7e8ad632c857047bddecc00697e25a3f4de98805
haroldCoder <haroldc2005@gmail.com>
2023-03-18 16:15:21 -0500

    net

--- MicroserviceNet_fruits/Controllers/FruitsController.cs
+++ MicroserviceNet_fruits/Controllers/FruitsController.cs
@@ -8,7 +8,7 @@ namespace FruitApi.Controllers
 {
     [Route("api/fruits")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class FruitsController : ControllerBase
     {
         public class Fruta
         {
